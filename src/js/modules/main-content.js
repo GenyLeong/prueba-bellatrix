8cb636941a62c6e2db3119d4e8373d4b74095253
GenyLeong <genesis.leon.carbajal@gmail.com>
2018-07-25 18:03:04 -0500

    text

--- src/js/modules/main-content.js
+++ src/js/modules/main-content.js
@@ -15,8 +15,8 @@ var nodes = {};
 
 // Compute the distinct nodes from the links.
 links.forEach(function(link) {
-    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source})
-    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target})
+    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source, image : link.image, type : link.type, parent : link.parent})
+    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target, image : link.image, type : link.type, parent : link.parent})
 });
 
 console.log(links)
@@ -24,11 +24,11 @@ console.log(links)
 var force = d3.layout.force()
     .nodes(d3.values(nodes))
     .links(links)
-    .size([width, height])
-    .linkDistance(height /4)
-    .charge(-300)
-    .theta(1)
-    .gravity(0.03)
+    .size([width/2, height/2])
+    .linkDistance(width/2)
+    .linkStrength(0.1)
+    .charge(-60)
+    .gravity(0.02)
     .on("tick", tick)
     .start();
 
@@ -36,7 +36,7 @@ var svg = d3.select("#area2").append("svg")
     .attr("width", width)
     .attr("height", height)
 
-var tip = d3.select("body")
+var tip = d3.select("#area2")
         .append("div")
         .attr("class", "tip");
     
@@ -61,7 +61,7 @@ var tip = d3.select("body")
           tip.attr('style', `visibility: visible; left: ${posX}px; top: ${posY}px`);
         };
       }
-      force.stop()    
+      force.stop()         
     }
         tip.hide = function(){
           tip.style("visibility", "hidden");
@@ -69,18 +69,18 @@ var tip = d3.select("body")
         }
 
 // build the arrow.
-svg.append("svg:defs").selectAll("marker")
-    .data(["end"])      // Different link/path types can be defined here
-  .enter().append("svg:marker")    // This section adds in the arrows
-    .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 6)
-    .attr("orient", "auto")
-    .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5")
+// svg.append("svg:defs").selectAll("marker")
+//     .data(["end"])      // Different link/path types can be defined here
+//   .enter().append("svg:marker")    // This section adds in the arrows
+//     .attr("id", String)
+//     .attr("viewBox", "0 -5 10 10")
+//     .attr("refX", 15)
+//     .attr("refY", -1.5)
+//     .attr("markerWidth", 6)
+//     .attr("markerHeight", 6)
+//     .attr("orient", "auto")
+//     .append("svg:path")
+//     .attr("d", "M0,-5L10,0L0,5")
     
 
 // add the links and the arrows
@@ -88,36 +88,40 @@ var path = svg.append("svg:g").selectAll("path")
     .data(force.links())
   .enter().append("svg:path")
 //    .attr("class", function(d) { return "link " + d.type; })
-    .attr("class", "link")
+    .attr("class", function(d) { return "link " + d.source.parent})
     .attr("marker-end", "url(#end)");
 
+    console.log(force.links())
 // define the nodes
 var node = svg.selectAll(".node")
     .data(force.nodes())
-  .enter().append("g")
-    .attr("class", "node")
+    .enter()
+    .append("g")
+    .attr("class", "node")    
     .call(force.drag);
 
 node.append("image")
-          .attr("xlink:href", function(d){              
-              console.log(d)
-                for (var i = 0; i < jsond3.length; i++) {
-                  if(d.index == i){
-                      // console.log(jsond3[i])
-                      return jsond3[i].image
-                  }
-                  else if(d.index == 0){
-                      return jsond3[0].image
-                  }    
-                }
-          })
-          .attr("x", -8)
-          .attr("y", -8)
+        //   .attr("xlink:href", function(d){              
+        //       console.log(d)
+        //         for (var i = 0; i < jsond3.length; i++) {
+        //           if(d.index == i){
+        //               // console.log(jsond3[i])
+        //               return jsond3[i].image
+        //           }
+        //           else if(d.index == 0){
+        //               return jsond3[0].image
+        //           }    
+        //         }
+        //   })
+          .attr("xlink:href", function(d){return d.image}) 
+          .attr("x", -10)
+          .attr("y", -10)
           .attr("class", "image")
           .attr("width", 16)
           .attr("height", 16)
           .on("mouseover", tip.show)
           .on("mouseout", tip.hide)
+          .on("click", click)
 
 // add the text 
 node.append("text")
@@ -138,12 +142,19 @@ function tick() {
             dr + "," + dr + " 0 0,1 " + 
             d.target.x + "," + 
             d.target.y;
-    });
+    })
 
     node
         .attr("transform", function(d) { 
           return "translate(" + d.x + "," + d.y + ")"; })        
 }
 
+function click(d){
+    d3.selectAll(".link"+ "."+d.parent).style("stroke", "black")
+
+    console.log(path[0])
+    console.log(d.parent)
+    
+}
 
 })();
